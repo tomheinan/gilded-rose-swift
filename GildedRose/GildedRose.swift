28c530db63f99695291b07c3e9fbddb72a422f08
Tom Heinan <theinan@onemedical.com>
2015-08-31 21:54:08 -0700

    Initial commit

---
+++ GildedRose/GildedRose.swift
@@ -0,0 +1,9 @@
+//
+//  GildedRose.swift
+//  GildedRose
+//
+//  Created by Tom Heinan on 8/31/15.
+//  Copyright © 2015 Tom Heinan. All rights reserved.
+//
+
+import Foundation
---
+++ GildedRose/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  GildedRose
+//
+//  Created by Tom Heinan on 8/31/15.
+//  Copyright © 2015 Tom Heinan. All rights reserved.
+//
+
+struct Item {
+    
+    var name: String
+    var sellIn: Int
+    var quality: Int
+    
+}
